2b2afd9fe999cc48ed79d5d5751a29446b82d090
Daniel Schadt <daniel.schadt@kit.edu>
2025-08-13 14:05:00 +0200

    ... and run surb benchmarks
    why did I add this filter?!

--- sphinx-benchmarks/run.sh
+++ sphinx-benchmarks/run.sh
@@ -4,7 +4,7 @@ echo "path_length,payload_size,onion_size" >sphinx_onion_sizes.csv
 for i in {1..5} ; do
     rm -rf "target/criterion/length_$i"
     mkdir -p "target/criterion/length_$i/"
-    SPHINX_MAX_PATH_LENGTH=$i cargo bench -- 'sphinx (creation|unwrap)'
+    SPHINX_MAX_PATH_LENGTH=$i cargo bench -- 'sphinx (creation|unwrap|surb)'
     mv "target/criterion/sphinx creation "* "target/criterion/length_$i"
     mv "target/criterion/sphinx unwrap "* "target/criterion/length_$i"
     mv "target/criterion/sphinx surb" "target/criterion/length_$i"
