4d343c84c2454919c037514467fd4245002a1629
Daniel Schadt <daniel.schadt@kit.edu>
2025-08-25 18:42:16 +0200

    reduce clone depth
    why was it 5 to begin with?

--- testbed-setup.sh
+++ testbed-setup.sh
@@ -10,6 +10,6 @@ fi
 PATCH_BASE="f8317f5a03bd5d7fb5a66f53730b00aeb03484a7"
 REPO="https://github.com/nymtech/nym"
 
-git clone --revision=$PATCH_BASE --depth=5 "$REPO"
+git clone --revision=$PATCH_BASE --depth=1 "$REPO"
 cd nym
 git apply ../testbed.patch
