3088fec2751f35d11f90996e6ca87e1deb1ad839
Daniel Schadt <daniel.schadt@kit.edu>
2026-06-18 09:31:13 +0200

    add better output to testbed-setup

--- testbed-setup.sh
+++ testbed-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+say() {
+    echo -e "\033[33m$1\033[0m"
+}
+
 if [ -e "nym" ] ; then
     echo "nym repository already cloned"
     echo "remove before continuing!"
@@ -10,12 +14,16 @@ fi
 PATCH_BASE="f8317f5a03bd5d7fb5a66f53730b00aeb03484a7"
 REPO="https://github.com/nymtech/nym"
 
+say "Attempting shallow clone"
 git clone --revision=$PATCH_BASE --depth=1 "$REPO" || (
-    echo "Revision clone did not work, falling back to normal clone"
+    say "Shallow clone did not work, falling back to normal clone"
     git clone "$REPO"
     cd nym
     git checkout $PATCH_BASE
 )
+say "Successfully got nym revision $PATCH_BASE"
 cd nym
+say "Applying patch"
 git apply ../testbed.patch
 mkdir shadow/outputs
+say "Testbed setup complete, you can now run ./testbed-run.sh"
